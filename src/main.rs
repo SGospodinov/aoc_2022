b9165eef655fbb5349690dd468a416baf395be6a
Stanislav Gospodinov <SGospodinov@users.noreply.github.com>
2022-12-01 11:11:23 +0000

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod day_1;
 use crate::utils::load_day_input;
 
 fn main() {
-    println!("🎄 Adevnt of Code 2022 🎁");
+    println!("🎄 Advent of Code 2022 🎁");
 
     println!("Day 1 - Part 1: {}", day_1::part_1(&load_day_input(1)));
     println!("Day 1 - Part 2: {}", day_1::part_2(&load_day_input(1)));
