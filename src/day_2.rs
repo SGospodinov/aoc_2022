6a0284f1c30858886477bd42236a7b4e20f799a7
Stanislav Gospodinov <SGospodinov@users.noreply.github.com>
2022-12-03 12:03:52 +0000

    Day 2 - part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -23,38 +23,89 @@ impl FromStr for RPS {
     }
 }
 
-fn parse_line(line: &str) -> (RPS, RPS) {
+#[derive(Debug,PartialEq)]
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl FromStr for Outcome {
+    type Err = std::string::ParseError;
+
+    fn from_str(outcome_str: &str) -> Result<Self, Self::Err> {
+        let outcome = match outcome_str {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => unreachable!("Invalid shape"),
+        };
+
+        Ok(outcome)
+    }
+}
+
+fn parse_line_part_1(line: &str) -> (RPS, RPS) {
     let (l, r) = line.split_once(SPACE).unwrap();
 
     (RPS::from_str(l).unwrap(), RPS::from_str(r).unwrap())
 }
 
-fn parse_input(input: &String) -> Vec<(RPS, RPS)> {
-    input.trim().lines().map(parse_line).collect()
+fn parse_input_part_1(input: &String) -> Vec<(RPS, RPS)> {
+    input.trim().lines().map(parse_line_part_1).collect()
 }
 
 fn get_socre(game: &(RPS, RPS)) -> u64 {
     match game {
-        (RPS::Rock,     RPS::Rock)      => 4, // 1 + 3,
-        (RPS::Rock,     RPS::Paper)     => 8, // 2 + 6,
-        (RPS::Rock,     RPS::Scissors)  => 3, // 3 + 0,
+        (RPS::Rock,     RPS::Rock)      => 4, // 1 + 3
+        (RPS::Rock,     RPS::Paper)     => 8, // 2 + 6
+        (RPS::Rock,     RPS::Scissors)  => 3, // 3 + 0
 
-        (RPS::Paper,    RPS::Rock)      => 1, // 1 + 0,
-        (RPS::Paper,    RPS::Paper)     => 5, // 2 + 3,
-        (RPS::Paper,    RPS::Scissors)  => 9, // 3 + 6,
+        (RPS::Paper,    RPS::Rock)      => 1, // 1 + 0
+        (RPS::Paper,    RPS::Paper)     => 5, // 2 + 3
+        (RPS::Paper,    RPS::Scissors)  => 9, // 3 + 6
 
-        (RPS::Scissors, RPS::Rock)      => 7, // 1 + 6,
-        (RPS::Scissors, RPS::Paper)     => 2, // 2 + 0,
-        (RPS::Scissors, RPS::Scissors)  => 6, // 3 + 3,
+        (RPS::Scissors, RPS::Rock)      => 7, // 1 + 6
+        (RPS::Scissors, RPS::Paper)     => 2, // 2 + 0
+        (RPS::Scissors, RPS::Scissors)  => 6, // 3 + 3
     }
 }
 
 pub fn part_1(input: &String) -> u64 {
-    parse_input(input).iter().map(get_socre).sum()
+    parse_input_part_1(input).iter().map(get_socre).sum()
+}
+
+
+fn get_geame(game_spec: &(RPS, Outcome)) -> (RPS, RPS) {
+    match game_spec {
+        (RPS::Rock, Outcome::Lose)     => (RPS::Rock, RPS::Scissors),
+        (RPS::Rock, Outcome::Draw)     => (RPS::Rock, RPS::Rock),
+        (RPS::Rock, Outcome::Win)      => (RPS::Rock, RPS::Paper),
+
+        (RPS::Paper, Outcome::Lose)    => (RPS::Paper, RPS::Rock),
+        (RPS::Paper, Outcome::Draw)    => (RPS::Paper, RPS::Paper),
+        (RPS::Paper, Outcome::Win)     => (RPS::Paper, RPS::Scissors),
+
+        (RPS::Scissors, Outcome::Lose) => (RPS::Scissors, RPS::Paper),
+        (RPS::Scissors, Outcome::Draw) => (RPS::Scissors, RPS::Scissors),
+        (RPS::Scissors, Outcome::Win ) => (RPS::Scissors, RPS::Rock),
+    }
+}
+
+fn parse_line_part_2(line: &str) -> (RPS, Outcome) {
+    let (l, r) = line.split_once(SPACE).unwrap();
+
+    (RPS::from_str(l).unwrap(), Outcome::from_str(r).unwrap())
+}
+
+fn parse_input_part_2(input: &String) -> Vec<(RPS, Outcome)> {
+    input.trim().lines().map(parse_line_part_2).collect()
 }
 
 pub fn part_2(input: &String) -> u64 {
-    11
+    parse_input_part_2(input)
+        .iter()
+        .map(|gs| get_socre(&get_geame(gs))).sum()
 }
 
 #[cfg(test)]
@@ -86,7 +137,14 @@ C Z";
     }
 
     #[test]
+    fn parses_outcomes() {
+        assert_eq!(Outcome::from_str("X"), Ok(Outcome::Lose));
+        assert_eq!(Outcome::from_str("Y"), Ok(Outcome::Draw));
+        assert_eq!(Outcome::from_str("Z"), Ok(Outcome::Win));
+    }
+
+    #[test]
     fn part_2() {
-        assert_eq!(super::part_2(&String::from(TEST_INPUT)), 10);
+        assert_eq!(super::part_2(&String::from(TEST_INPUT)), 12);
     }
 }
