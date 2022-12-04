9c0fbaf068761a2d408039737f0c59d33a68ba90
Stanislav Gospodinov <SGospodinov@users.noreply.github.com>
2022-12-04 08:59:02 +0000

    Day 4 (#4)
    * Day 4 - part 1
    
    * Day 4 - part 2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod utils;
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 use crate::utils::load_day_input;
 
@@ -16,4 +17,6 @@ fn main() {
     println!("Day 2 - Part 2: {}", day_2::part_2(&load_day_input(2)));
     println!("Day 3 - Part 1: {}", day_3::part_1(&load_day_input(3)));
     println!("Day 3 - Part 2: {}", day_3::part_2(&load_day_input(3)));
+    println!("Day 4 - Part 1: {}", day_4::part_1(&load_day_input(4)));
+    println!("Day 4 - Part 2: {}", day_4::part_2(&load_day_input(4)));
 }
