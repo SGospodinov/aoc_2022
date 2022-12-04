4240823c8512972d1852dfd358b2b3f3981027eb
Stanislav Gospodinov <SGospodinov@users.noreply.github.com>
2022-12-04 08:47:02 +0000

    Day 4 - part 1

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,8 @@ use std::fs::read_to_string;
 pub mod constants {
     pub const EMPTY_LINE: &str = "\n\n";
     pub const SPACE: &str = " ";
+    pub const COMMA: &str = ",";
+    pub const HYPHEN: &str = "-";
 }
 
 pub fn load_day_input(day: u8) -> String {
